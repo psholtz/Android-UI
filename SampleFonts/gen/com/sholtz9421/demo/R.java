e7f3c455c9e53779337088509dc6e3f1d8e3470b
psholtz <paul.sholtz@gmail.com>
2011-06-27 12:40:36 -0700

    adding ui

--- SampleFonts/gen/com/sholtz9421/demo/R.java
+++ SampleFonts/gen/com/sholtz9421/demo/R.java
@@ -10,17 +10,31 @@ package com.sholtz9421.demo;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blueColor=0x7f050002;
+        public static final int cyanColor=0x7f050004;
+        public static final int greenColor=0x7f050000;
+        public static final int purpleColor=0x7f050005;
+        public static final int redColor=0x7f050001;
+        public static final int yellowColor=0x7f050003;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int layout1=0x7f050000;
-        public static final int textView1=0x7f050001;
-        public static final int textView2=0x7f050003;
-        public static final int textView3=0x7f050002;
-        public static final int textView4=0x7f050005;
-        public static final int textView5=0x7f050004;
-        public static final int textView6=0x7f050006;
+        public static final int layout1=0x7f060000;
+        public static final int textView1=0x7f060001;
+        public static final int textView10=0x7f060004;
+        public static final int textView11=0x7f060009;
+        public static final int textView12=0x7f06000c;
+        public static final int textView2=0x7f060005;
+        public static final int textView3=0x7f060002;
+        public static final int textView4=0x7f060007;
+        public static final int textView5=0x7f060006;
+        public static final int textView6=0x7f06000a;
+        public static final int textView7=0x7f060003;
+        public static final int textView8=0x7f060008;
+        public static final int textView9=0x7f06000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- SampleFonts/src/com/sholtz9421/demo/SampleFonts.java
+++ SampleFonts/src/com/sholtz9421/demo/SampleFonts.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class SampleFonts extends Activity {
 	private RelativeLayout layout1;
+	
 	private TextView text1;
 	private TextView text2; 
 	private TextView text3; 
@@ -18,12 +19,26 @@ public class SampleFonts extends Activity {
 	private TextView text5;
 	private TextView text6;
 	
+	private TextView text7;
+	private TextView text8;
+	private TextView text9;
+	private TextView text10;
+	private TextView text11;
+	private TextView text12; 
+	
 	private Typeface tf1; 
 	private Typeface tf2; 
 	private Typeface tf3; 
 	private Typeface tf4; 
 	private Typeface tf5; 
 	private Typeface tf6;
+	
+	private Typeface tf7;
+	private Typeface tf8;
+	private Typeface tf9;
+	private Typeface tf10;
+	private Typeface tf11;
+	private Typeface tf12;
 	
     /** Called when the activity is first created. */
     @Override
@@ -37,40 +52,68 @@ public class SampleFonts extends Activity {
         layout1 = (RelativeLayout)inflater.inflate(R.layout.main, null);
         Log.v(Model.TAG,"*** layout: " + layout1); 
         
+        // FIRST SIX SLOTS 
         text1 = (TextView)findViewById(R.id.textView1);
-        text1.setBackgroundColor(Color.parseColor("#014401"));
         tf1 = Typeface.createFromAsset(getAssets(),"fonts/File.otf");
         text1.setTypeface(tf1); 
         text1.setText("File");
         
         text2 = (TextView)findViewById(R.id.textView2);
-        text2.setBackgroundColor(Color.parseColor("#440101")); 
         tf2 = Typeface.createFromAsset(getAssets(),"fonts/AmputaBangiz.ttf");
         text2.setTypeface(tf2); 
         text2.setText("Amputa\nBangiz");
         
         text3 = (TextView)findViewById(R.id.textView3);
-        text3.setBackgroundColor(Color.parseColor("#010144")); 
         tf3 = Typeface.createFromAsset(getAssets(),"fonts/Quicksand_Dash.otf");
         text3.setTypeface(tf3); 
         text3.setText("Quicksand"); 
         
         text4 = (TextView)findViewById(R.id.textView4);
-        text4.setBackgroundColor(Color.parseColor("#444401"));
         tf4 = Typeface.createFromAsset(getAssets(), "fonts/QuadLight.otf");
         text4.setTypeface(tf4);
         text4.setText("Quad");
         
         text5 = (TextView)findViewById(R.id.textView5);
-        text5.setBackgroundColor(Color.parseColor("#014444"));
         tf5 = Typeface.createFromAsset(getAssets(), "fonts/orbitron-black.otf");
         text5.setTypeface(tf5);
         text5.setText("Orbitron");
         
         text6 = (TextView)findViewById(R.id.textView6);
-        text6.setBackgroundColor(Color.parseColor("#440144"));
         tf6 = Typeface.createFromAsset(getAssets(), "fonts/Dubtronic-Solid.otf");
         text6.setTypeface(tf6); 
         text6.setText("Dubtronic"); 
+        
+        // NEXT SIX SLOTS
+        text7 = (TextView)findViewById(R.id.textView7);
+        text7.setText("Sliced AB");
+        tf7 = Typeface.createFromAsset(getAssets(), "fonts/Sliced-AB.ttf");
+        text7.setTypeface(tf7);
+        
+        text8 = (TextView)findViewById(R.id.textView8);
+        text8.setText("Paranoid");
+        tf8 = Typeface.createFromAsset(getAssets(), "fonts/Paranoid.otf");
+        text8.setTypeface(tf8); 
+       
+        text9 = (TextView)findViewById(R.id.textView9); 
+        text9.setText("Cranberry");
+        tf9 = Typeface.createFromAsset(getAssets(), "fonts/CranberryBlues.ttf");
+        text9.setTypeface(tf9); 
+
+        text10 = (TextView)findViewById(R.id.textView10);
+        text10.setText("Drew Cap");
+        tf10 = Typeface.createFromAsset(getAssets(), "fonts/Drew-Cap.ttf");
+        text10.setTypeface(tf10); 
+
+        text11 = (TextView)findViewById(R.id.textView11);
+        text11.setText("Exus"); 
+        tf11 = Typeface.createFromAsset(getAssets(), "fonts/Exus.ttf");
+        text11.setTypeface(tf11); 
+
+        text12 = (TextView)findViewById(R.id.textView12);
+        text12.setText("Stahl Beton");
+        tf12 = Typeface.createFromAsset(getAssets(), "fonts/stahlbeton.otf");
+        text12.setTypeface(tf12); 
+
+        Log.v(Model.TAG,"*** starting app"); 
     }
 }
